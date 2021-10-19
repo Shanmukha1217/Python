4b9f1a71bcb809aac52df7bdbf9a935d83897400
Shanmukha <shanmukhads7@gmail.com>
2021-10-19 10:22:15 +0530

    initial commit :wq

---
+++ funsions/fact.py
@@ -0,0 +1,19 @@
+# def fact():
+#     num = int(input('enter a num: '))
+#     res = int(1)
+
+#     for i in range(num,1 ,-1):
+#         res ;*= i
+#     print(f'The factorial of a number{num} is {res}')
+
+# fact()
+
+def factorial(num):
+    fact = 1
+    for n in range(num,1,-1):
+        fact *= n
+    print(f'{num}! = {fact}')\
+
+factorial(5)
+factorial(8)
+factorial(13)
