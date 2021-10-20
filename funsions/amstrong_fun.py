5eb5ea91286263fbf858b3597b54b44ddb852acb
Shanmukha <shanmukhads7@gmail.com>
2021-10-20 22:04:59 +0530

    changes added

--- funsions/amstrong_fun.py
+++ funsions/amstrong_fun.py
@@ -1,29 +1,31 @@
-def main():
-    res = 0
-    num = int(input('enter a num: '))
-    if is_amstrong(num):
-        print('is amstrong')
-    else:
-        print('is not amstrong')
 
-def no_of_factors(num:int)->int:
+def main():
+    num = int(input('enter a num: '))
+    is_amstrong(num)
+
+
+def is_amstrong(num:int)-> bool:
+    if result(num) == num:
+        return print(f'{num} is a amstrong')
+    return print(f'{num} is not amstrong')
+
+
+def no_of_digits(num:int)->int:
     count = 0
-    while temp:
-        temp //= 10
+    while num:
+        num //= 10
         count += 1
     return count
 
 
 def result(num:int)-> int:
     temp = num
-    if no_of_factors:
-        while temp:
-            last_digit = temp % 10
-            res += (last_digit ** count)
-            temp //= 10
-        return res
-
-
+    res = 0
+    while temp:
+        last_digit = temp % 10
+        res += (last_digit ** no_of_digits(num))
+        temp //= 10
+    return res
 
 
 main()
